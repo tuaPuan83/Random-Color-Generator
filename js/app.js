0a96cc7aa2ded9966c4d08a7eea6b3eb432e9769
William Klaiwonpen <williamklaiwonpen@gmail.com>
2024-09-16 21:15:06 +0700

    Finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const colorBox = document.querySelector('#color__square');
-const colorText = document.querySelector('#color__name');
+// const colorBox = document.querySelector('#color__square');
+
 const generatorBtn = document.querySelector('#ran__btn');
 const bgColorBtn = document.querySelector('#ran__bg__color');
 
@@ -10,28 +10,55 @@ const generatorPalette = () => {
         let randomColor = Math.floor(Math.random() * 0xffffff).toString(16);
         let ranNum = randomColor.padStart(6,0);
 
-        colorBox.style.background = `#${ranNum}`;
-        colorText.innerHTML = `#${ranNum}`;
+        // colorBox.style.background = `#${ranNum}`;
+        // colorText.innerHTML = `#${ranNum}`;
     }
     
 }
 
 generatorBtn.addEventListener('click', generatorPalette);
 
-const ranBgColor = () => {
+// const ranBgColor = () => {
+//     const r = Math.floor(Math.random() * 256)
+//     const g = Math.floor(Math.random() * 256)
+//     const b = Math.floor(Math.random() * 256)
+
+//     const rgb = "rgb(" + r + ", " + g + ", " + b +")";
+
+//     console.log(rgb)
+
+//     document.body.style.backgroundColor = rgb
+
+// }
+
+// bgColorBtn.addEventListener('click', ranBgColor)
+
+const makeRandColor = () => {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
-    const b = Math.floor(Math.random() * 256)
-
-    const rgb = "rgb(" + r + ", " + g + ", " + b +")";
-
-    console.log(rgb)
-
-    document.body.style.backgroundColor = rgb
-
+    const b = Math.floor(Math.random() * 256) 
+    return `rgb(${r},${g},${b})`;
 }
 
-bgColorBtn.addEventListener('click', ranBgColor)
+const boxes = document.querySelectorAll('#color__square')
+const colorTexts = document.querySelectorAll('#color__name');
+
+for (let box of boxes) {
+    generatorBtn.addEventListener('click', function () {
+        box.style.background = makeRandColor();
+    })
+}
+
+for (let colorText of colorTexts) {
+    generatorBtn.addEventListener('click', function () {
+        // box.style.background = makeRandColor();
+        colorText.innerHTML = makeRandColor();
+    })
+}
+
+
+
+console.log(boxes)
 
 
 

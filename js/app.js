240632767dcf01d8288341d6f8cf89f7c68c8c97
William Klaiwonpen <williamklaiwonpen@gmail.com>
2024-09-16 14:56:43 +0700

    Finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const colorBox = document.querySelector('#color__square');
 const colorText = document.querySelector('#color__name');
 const generatorBtn = document.querySelector('#ran__btn');
+const bgColorBtn = document.querySelector('#ran__bg__color');
+
+
 
 const generatorPalette = () => {
     for (let i = 0; i < 8; i++) {
@@ -14,6 +17,21 @@ const generatorPalette = () => {
 }
 
 generatorBtn.addEventListener('click', generatorPalette);
+
+const ranBgColor = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    const rgb = "rgb(" + r + ", " + g + ", " + b +")";
+
+    console.log(rgb)
+
+    document.body.style.backgroundColor = rgb
+
+}
+
+bgColorBtn.addEventListener('click', ranBgColor)
 
 
 
